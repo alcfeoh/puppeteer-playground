c9a4ce3e7f3f1d0545784aff67d71804a89c536a
Alain Chautard <achautard@gmail.com>
2018-08-28 17:14:11 -0700

    Added flight entry (in progress)

--- flight-memory.js
+++ flight-memory.js
@@ -4,25 +4,60 @@ async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await logIn(page, 'test', 'password');
-    await getScreenshotOfStats(page);
+    await logIn(page, 'test', 'demo');
+    console.log('in');
+    await enterNewFlight(page, {
+        departureCode: 'SMF',
+        arrivalCode: 'CDG',
+        departureDate: '2019-06-13',
+        departureTime: '15:46',
+        arrivalDate: '2019-06-14',
+        arrivalTime: '08:34'
+    });
+    await page.screenshot({path: 'flightmemory.png', fullPage: true});
     await browser.close();
 }
 
 async function logIn(page, username, password) {
     await page.goto('https://www.flightmemory.com/');
-    await page.content();
     await page.click("input[name='username']");
     await page.keyboard.type(username);
     await page.click("input[name='passwort']");
     await page.keyboard.type(password);
-    await page.click("input[type='submit']");
-    return page.content();
+    return page.click("input[type='submit']");
 }
 
 async function getScreenshotOfStats(page) {
     await page.goto('https://www.flightmemory.com/signin/?go=statistik');
-    await page.content();
-    return page.screenshot({path: 'flightmemory.png', fullPage: true})
+    return page.screenshot({path: 'flightmemory.png', fullPage: true});
 }
 
-run();
+async function enterNewFlight(page, flightInfo) {
+    await page.goto('https://www.flightmemory.com/signin/?go=flugdaten_neu');
+    await page.content();
+    console.log('here');
+    await page.click("input[name='ab_3letter']");
+    await page.keyboard.type(flightInfo.departureCode);
+    await page.click("input[name='an_3letter']");
+    await page.keyboard.type(flightInfo.arrivalCode);
+    console.log('apts');
+    await page.click("input[name='ab_datum_eingabe']");
+    await page.keyboard.type(flightInfo.departureDate);
+    console.log('date1');
+    await page.click("input[name='an_datum_eingabe']");
+    await page.keyboard.type(flightInfo.arrivalDate);
+    await page.click("input[name='ab_zeit_eingabe']");
+    console.log('dates');
+    await page.keyboard.type(flightInfo.departureTime);
+    await page.click("input[name='an_zeit_eingabe']");
+    await page.keyboard.type(flightInfo.arrivalTime);
+    console.log('done');
+    return page.click("input[type='submit']");
+}
+
+try {
+    run();
+} catch(error) {
+    console.error(error);
+}
+
